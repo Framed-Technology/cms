14843f1e6de2ec1e3e1176d962e1d9814777ac29
Jack Skerman <jack.skerm@gmail.com>
2024-03-01 16:29:26 +0100

    article

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,39 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    // ~/strapi-aws-s3/backend/config/middlewares.js
+    
+    module.exports = [
+      'strapi::errors',
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'yourBucketName.s3.yourRegion.amazonaws.com',
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'yourBucketName.s3.yourRegion.amazonaws.com',
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
+      'strapi::cors',
+      'strapi::poweredBy',
+      'strapi::logger',
+      'strapi::query',
+      'strapi::body',
+      'strapi::session',
+      'strapi::favicon',
+      'strapi::public',
+    ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,23 @@
-module.exports = () => ({});
+    // ~/strapi-aws-s3/backend/config/plugins.js
+    
+    module.exports = ({ env }) => ({
+        upload: {
+          config: {
+            provider: 'aws-s3',
+            providerOptions: {
+              accessKeyId: env('R2_ACCESS_KEY'),
+              secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+              params: {
+                ACL: env('AWS_ACL', 'public-read'),
+                signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+                Bucket: env('AWS_BUCKET'),
+              },
+            },
+            actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
+            },
+          },
+        },
+  });
